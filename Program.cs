460d5e1e4fb247e833295370e09e668ea1a91456
Dipakshi Saxena <75835612+Dipakshi26@users.noreply.github.com>
2022-06-21 17:01:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+
+    public class student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string dept { get; set; }
+
+    }
+    public class Program
+    {
+        public void show()
+        {
+            List<student> list = new List<student>()
+         {
+             new student (){ id = 1, name ="Dipakshi", age=22, dept="IT"},
+             new student (){ id = 1, name = "Shatakshi", age=23, dept="IT"},
+             new student (){ id = 1, name = "Aditya", age=22, dept="IT"},
+             new student (){ id = 1, name = "Ayush", age=21, dept="CSE"},
+             new student (){ id = 1, name = "Akshat", age=23, dept="IT"},
+             new student (){ id = 1, name = "Prashant", age=21, dept="Civil"},
+
+         };
+            var result = (from l in list
+                          where l.dept == "IT"
+                          select l);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.id);
+                Console.WriteLine(item.name);
+                Console.WriteLine(item.age);
+                Console.WriteLine(item.dept);
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.show();
+        }
+    }
+}
---
+++ employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinuxhandsOn
+{
+    public class employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string dept { get; set; }
+
+    }
+    internal class employeedetails
+    {
+        public void emp()
+        {
+            List<employee> list = new List<employee>()
+         {
+             new employee (){ id = 1, name = "Dipakshi", age=22, dept="IT"},
+             new employee (){ id = 1, name = "Shatakshi", age=23, dept="IT"},
+             new employee (){ id = 1, name = "Aditya", age=22, dept="IT"},
+             new employee (){ id = 1, name = "Ayush", age=21, dept="CSE"},
+             new employee (){ id = 1, name = "Akshat", age=23, dept="IT"},
+             new employee (){ id = 1, name = "Prashant", age=21, dept="Civil"},
+
+         };
+            var result1 = (from e in list
+                           where e.dept == "IT"
+                           select e).Single();
+            Console.WriteLine(result1);
+
+        }
+    }
+}
